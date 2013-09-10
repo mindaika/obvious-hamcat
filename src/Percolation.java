522bb5e2669c9d82ec3378dc4ed7138e30490252
Randall Sewell <randallsewell@gmail.com>
2013-09-10 08:50:46 -0700

    First commit

---
+++ src/Percolation.java
@@ -0,0 +1,55 @@
+public class Percolation
+{
+    // ...
+    // your data members here
+    private WeightedQuickUnionUF Unity;
+    private int GridEdge;
+    private boolean[][] Grid;
+    // ...
+
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N)
+    {
+        Unity = new WeightedQuickUnionUF((N*N)); // Which sites are connected
+        GridEdge = N;                            // Grid Size
+        Grid = new boolean[N][N];                // Which sites are open
+    }
+
+
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j)
+    {
+
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j)
+    {
+        //System.out.println(gridToIndex(i, j));
+        return false;
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j)
+    {
+        return false;
+    }
+
+    // does the system percolate?
+    public boolean percolates()
+    {
+        return false;
+    }
+
+    private int gridToIndex(int row, int col)
+    {
+        int index = col + ((row - 1) * GridEdge);
+        return index;
+    }
+
+    private boolean indexValidator(int index)
+    {
+        boolean indexIsValid = false;
+        if (i <= 0 || i > N) throw new IndexOutOfBoundsException("row index i out of bounds");
+    }
+}
---
+++ src/PercolationStats.java
@@ -0,0 +1,35 @@
+public class PercolationStats
+{
+    // ...
+    // your data members here
+    // ...
+
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T)
+    {
+    }
+
+    // sample mean of percolation threshold
+    public double mean()
+    {
+        return 0.0;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev()
+    {
+        return 0.0;
+    }
+
+    // returns lower bound of the 95% confidence interval
+    public double confidenceLo()
+    {
+        return 0.0;
+    }
+
+    // returns upper bound of the 95% confidence interval
+    public double confidenceHi()
+    {
+        return 0.0;
+    }
+}
