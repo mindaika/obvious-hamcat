23f11a360ab225979ce2916cbee72ddad8c17d18
Randall Sewell <randallsewell@gmail.com>
2013-09-11 21:58:44 -0700

    Two tests remaining

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,10 +2,11 @@ public class Percolation
 {
     // ...
     // your data members here
-    private WeightedQuickUnionUF Unity;
-    private int GridEdge;
-    private boolean[][] Grid;
-    private int TopIndex, BottomIndex;
+    private WeightedQuickUnionUF unity, workAbout;
+    private boolean[] siteIsFull;
+    //private boolean[][] siteIsFull2;
+    private boolean[][] grid;
+    private int topIndex, bottomIndex, gridEdge;
     // ...
 
     // create N-by-N grid, with all sites blocked
@@ -14,85 +15,135 @@ public class Percolation
         // Which sites are connected;
         // Index N^2 is the top virtual site
         // Index N^2+1 is the bottom virtual site
-        Unity = new WeightedQuickUnionUF((N*N)+2);
-        TopIndex = (N*N);
-        BottomIndex = (N*N+1);
+        unity = new WeightedQuickUnionUF((N*N)+2);
+        workAbout = new WeightedQuickUnionUF(N*N+1);
+        topIndex = (N*N);
+        bottomIndex = (N*N+1);
 
-        // Grid Edge size. Total grid size is N^2
-        GridEdge = N;
+        // grid Edge size. Total grid size is N^2
+        gridEdge = N;
 
-        // Grid to store which sites are open
-        Grid = new boolean[N][N];
+        // grid to store which sites are open, full
+        grid = new boolean[N][N];
+        siteIsFull = new boolean[N*N];
+        //siteIsFull2 = new boolean[N][N];
 
         // Join top virtual site to top row
-        for (int i = 0; i < N; i++)
-            Unity.union(TopIndex, gridToIndex(0,i));
+        for (int i = 0; i < N; i++) {
+            unity.union(topIndex, gridToIndex(0, i));
+            workAbout.union(topIndex, gridToIndex(0, i));
+        }
 
         // Join virtual bottom to bottom row
         for (int i = 0; i < N; i++)
-            Unity.union(gridToIndex(N-1, i),BottomIndex);
+            unity.union(gridToIndex(N-1, i), bottomIndex);
     }
 
     // open site (row i, column j) if it is not already
     public void open(int i, int j)
     {
-        if (!Grid[i-1][j-1])
+        int siteIndex = gridToIndex(i-1, j-1);
+        indexValidator(siteIndex);
+        if (!grid[i-1][j-1])
         {
-            int siteIndex = gridToIndex(i-1, j-1);
-            indexValidator(siteIndex);
-
             // Open site
-            Grid[i-1][j-1] = true;
-
+            grid[i-1][j-1] = true;
+/*
+            // Open Top-row sites are always full
+            if (i == 1 && grid[i-1][j-1])
+                siteIsFull[siteIndex] = true;
+*/
             // Join site to left
-            if (j != 1 && Grid[i-1][j-1])
-                Unity.union(siteIndex, siteIndex-1);
+            if (j != 1 && grid[i-1][j-2]) {
+                unity.union(siteIndex, siteIndex - 1);
+                workAbout.union(siteIndex, siteIndex - 1);
+            }
 
             // Join site to right
-            if (j != GridEdge && Grid[i-1][j])
-                Unity.union(siteIndex, siteIndex+1);
+            if (j != gridEdge && grid[i-1][j]) {
+                unity.union(siteIndex, siteIndex + 1);
+                workAbout.union(siteIndex, siteIndex + 1);
+            }
 
             // Join site below, unless bottom row
-            if (i != GridEdge && Grid[i][j-1])
-                Unity.union(siteIndex, siteIndex+GridEdge);
+            if (i != gridEdge && grid[i][j-1]) {
+                unity.union(siteIndex, siteIndex + gridEdge);
+                workAbout.union(siteIndex, siteIndex + gridEdge);
+            }
 
             // Join site above, unless top row
-            if (i != 1 && Grid[i-2][j-1])
-                Unity.union(siteIndex, siteIndex-GridEdge);
+            if (i != 1 && grid[i-2][j-1]) {
+                unity.union(siteIndex, siteIndex - gridEdge);
+                workAbout.union(siteIndex, siteIndex - gridEdge);
+            }
         }
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j)
     {
-        return Grid[i-1][j-1];
+        return grid[i-1][j-1];
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j)
     {
-        return (Unity.connected(gridToIndex(i-1, j-1),TopIndex) &&
-                Grid[i-1][j-1]);
+        int siteIndex = gridToIndex(i-1, j-1);
+        indexValidator(siteIndex);
+
+        // A site is full if connected to the topIndex and isOpen
+        if (workAbout.connected(gridToIndex(i-1, j-1), topIndex)
+                && grid[i-1][j-1])
+                //&& !unity.connected(gridToIndex(i-1, j-1), bottomIndex))
+            siteIsFull[siteIndex] = true;
+
+        // Check site to left
+        if (j != 1 && workAbout.connected(gridToIndex(i-1, j-2), topIndex) && grid[i-1][j-1])
+        //if (j != 1 && siteIsFull[i-1][j-2] && grid[i-1][j-1])
+            siteIsFull[siteIndex] = true;
+
+
+        // Check site to right
+        if (j != gridEdge && workAbout.connected(gridToIndex(i-1, j), topIndex) && grid[i-1][j-1])
+        //if (j != gridEdge && siteIsFull[i-1][j] && grid[i-1][j-1])
+            siteIsFull[siteIndex] = true;
+
+        // Check site below, unless bottom row
+        //if (i != gridEdge && unity.connected(gridToIndex(i, j-1), topIndex) && grid[i-1][j-1])
+        if (i != gridEdge && siteIsFull[siteIndex] && grid[i-1][j-1])
+            siteIsFull[siteIndex] = true;
+
+        // Check site above, unless top row
+        if (i != 1 && siteIsFull[siteIndex] && grid[i-1][j-1])
+        //if (i != 1 && workAbout.connected(gridToIndex(i-2, j-1), topIndex) && grid[i-1][j-1])
+            siteIsFull[siteIndex] = true;
+
+        return (siteIsFull[siteIndex]);
     }
 
     // does the system percolate?
     public boolean percolates()
     {
-        return Unity.connected(TopIndex,BottomIndex);
+        boolean perky = false;
+        if (gridEdge == 1 && !grid[0][0])
+            perky = false;
+        else
+            perky = unity.connected(topIndex, bottomIndex);
+
+        return perky;
     }
 
     // Convert grid location to index in boolean array
     private int gridToIndex(int row, int col)
     {
-        int index = col + (row * GridEdge);
+        int index = col + (row * gridEdge);
         return index;
     }
 
     // Validates some give index, but I don't think I actually used it anywhere
     private boolean indexValidator(int index)
     {
-        boolean indexIsValid = true;
-        if (index < 0 || index > GridEdge*GridEdge+2) throw new IndexOutOfBoundsException("row index i out of bounds");
-        return indexIsValid;
+        if (index < 0 || index > gridEdge * gridEdge+1) throw new IndexOutOfBoundsException("row index i out of bounds");
+        return true;
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,34 +2,67 @@ public class PercolationStats
 {
     // ...
     // your data members here
+    private int gridSize, trials, totalSites;
+    private double[] thatsJustMean;
     // ...
 
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T)
     {
+        if (N <= 0 || T <= 0)
+            throw new IllegalArgumentException("Error, Will Robinson...");
+        gridSize = N;
+        trials = T;
+        totalSites = N * N;
+        thatsJustMean = new double[T];
+
+        for (int index = 0; index < trials; index++) {
+            Percolation perc = new Percolation(gridSize);
+            double timeToPerk = 0;
+            while (!perc.percolates()) {
+                int i = StdRandom.uniform(gridSize) + 1;
+                int j = StdRandom.uniform(gridSize) + 1;
+                if (!perc.isOpen(i, j)) {
+                    perc.open(i, j);
+                    timeToPerk++;
+                }
+            }
+            thatsJustMean[index] = (timeToPerk / totalSites);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Stopwatch timer = new Stopwatch();
+        PercolationStats perkStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        StdOut.println("mean                    = " + perkStats.mean());
+        StdOut.println("stddev                  = " + perkStats.stddev());
+        StdOut.println("95% confidence interval = " + perkStats.confidenceLo() + ", " + perkStats.confidenceHi());
+        StdOut.println("It took: " + timer.elapsedTime() + " seconds");
     }
 
     // sample mean of percolation threshold
     public double mean()
     {
-        return 0.0;
+        return StdStats.mean(thatsJustMean);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev()
     {
-        return 0.0;
+        return StdStats.stddev(thatsJustMean);
     }
 
     // returns lower bound of the 95% confidence interval
     public double confidenceLo()
     {
-        return 0.0;
+        return (StdStats.mean(thatsJustMean) - StdStats.stddev(thatsJustMean));
     }
 
     // returns upper bound of the 95% confidence interval
     public double confidenceHi()
     {
-        return 0.0;
+        return (StdStats.mean(thatsJustMean) + StdStats.stddev(thatsJustMean));
     }
 }
